90f5403ef36dfdef9a5a6fdfb0f0f3635c96f4d2
ArnavPuri <arnavanytime@gmail.com>
2025-07-31 23:53:36 +0530

    Initial Commit

---
+++ notes&dotes/notes_dotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_dotesApp.swift
+//  notes&dotes
+//
+//  Created by Arnav Puri on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct notes_dotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
